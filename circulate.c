639f81f6ff6931123e31b40d1bb234bb71d3878a
MiddlePoYinJi <140052674+MiddlePoYinJi@users.noreply.github.com>
2023-07-22 18:39:40 +0800

    Add files via upload

---
+++ circulate.c
@@ -0,0 +1,17 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+#include<string.h>
+
+
+
+
+
+int main() {
+	int num = 1;
+	while (num < 5) {
+		printf("%d\n", num);
+		num++;
+	}
+	return 0;
+}
